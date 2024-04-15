6f57709cbd8c3371f78066c13bfa38a0ef811595
Informatica <informatica@alumno.local>
2024-04-15 11:47:03 -0600

    start v1.1.1'

--- frontend/src/components/input.jsx
+++ frontend/src/components/input.jsx
@@ -2,13 +2,48 @@ export const input = ({
     field,
     label,
     value,
-    onChangeHandler,
+    onChangeHandler, //manejar cambios
     type,
     showErrorMessage,
     validationMenssage,
-    onBlurHandler
+    onBlurHandler, //para hacer focus
+    textarea
 }) => {
-  return (
-    <div>input</div>
-  )
+    const handleValueChange = (event) => {
+        onChangeHandler(event.target.value, field)
+    }
+
+    const handleInputBlur = (event) =>{
+        onBlurHandler(event.target.value, field)
+    }
+    return (
+        <>
+            <div className="auth-form-label">
+                <span>{label}</span>
+            </div>
+            <div >
+                {textarea?(
+                    <textarea
+                        type={type}
+                        value={value}
+                        onChange={handleValueChange}
+                        onBlur={handleInputBlur}
+                        rows={5}
+                        style={{maxWidth: '400px'}}
+                    />
+                ) : (
+                    <input
+                        type={type}
+                        value={value}
+                        onChange={handleValueChange}
+                        onBlur={handleInputBlur}
+                    />
+                )
+                }
+                <span className="auth-form-validation-message">
+                    {showErrorMessage && validationMenssage}
+                </span>
+            </div>
+        </>
+    )
 }
